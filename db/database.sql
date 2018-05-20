88bc1ba027f0261840358eccabb062083ff99da8
Tom <tcantinelli@gmail.com>
2018-05-20 16:51:06 +0200

    Fichier database.sql OK

--- db/database.sql
+++ db/database.sql
@@ -12,43 +12,16 @@ create table if not exists USERS(
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   username VARCHAR(20),
   password VARCHAR(1024),
+  type VARCHAR(5),
   email VARCHAR(320)
 );
 
-/* Création de la table CARTES */
-create table if not exists CARTES(
+/* Création de la table MESSAGES */
+create table if not exists MESSAGES(
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  id_carte INT(20),
-  nom VARCHAR(20),
-  description VARCHAR(1024)
-);
-
-/* Création de la table RESTAURANT */
-create table if not exists RESTAURANT(
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  nom VARCHAR(20),
-  adresse VARCHAR(1024),
-  tel VARCHAR(10),
-  email VARCHAR(320)
-);
-
-/* Création de la table MENU */
-create table if not exists MENU(
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  id_carte INT,
-  nom VARCHAR(1024),
-  description VARCHAR(1024),
-  url VARCHAR(1024)
-);
-
-/* Création de la table PLATS */
-create table if not exists PLATS(
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  id_menu INT,
-  id_type INT,
-  prix FLOAT(4,2),
-  nom VARCHAR (128),
-  url VARCHAR(1024)
+  id_username INT,
+  date_creation DATE,
+  message VARCHAR(256)
 );
 
 /* Création de la table TYPES_DE_PLATS */
@@ -57,14 +30,70 @@ create table if not exists TYPES_DE_PLATS(
   nom VARCHAR(20)
 );
 
-/*Insertion des données - Jeu de test*/
+/* Création de la table PLATS */
+create table if not exists PLATS(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_type INT,
+  prix FLOAT(4,2),
+  nom VARCHAR (128),
+  url VARCHAR(1024)
+);
 
-/* Type de plats*/
-insert into TYPES_DE_PLATS (nom) VALUES('entrée');
-insert into TYPES_DE_PLATS (nom) VALUES('plat');
-insert into TYPES_DE_PLATS (nom) VALUES('dessert');
+/* Création de la table MENU */
+create table if not exists MENUS(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(128),
+  description VARCHAR(1024),
+  liste_plats VARCHAR(128)
+);
+
+/* Création de la table CARTES */
+create table if not exists CARTES(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(20),
+  liste_menus VARCHAR(128),
+  date_creation DATE
+);
+
+/* Insertion des données - Jeu de test */
+
+/* Users */
+insert into USERS (username,password,type,email) VALUES("administrateur","aaaa","admin","admin@restocoin.com");
+insert into USERS (username,password,type,email) VALUES("cuisto","aaaa","admin","cuisto@restocoin.com");
+insert into USERS (username,password,type,email) VALUES("jean","bbbb","user","jean@toto.com");
+insert into USERS (username,password,type,email) VALUES("jeanne","cccc","user","jeanne@hotmail.com");
+
+/* Messages */
+insert into MESSAGES (id_username,date_creation,message) VALUES(3,"2017-12-22","Super restaurant, l'équipe est très sympa et les plats délicieux");
+insert into MESSAGES (id_username,date_creation,message) VALUES(4,"2017-10-09","Je recommande vivement");
+
+/* Type de plats */
+insert into TYPES_DE_PLATS (nom) VALUES("Entrée");
+insert into TYPES_DE_PLATS (nom) VALUES("Plat");
+insert into TYPES_DE_PLATS (nom) VALUES("Dessert");
 
 /* Plats */
-insert into PLATS (id_type,nom,prix) VALUES(1,'Salade grecque',6.50):
-insert into PLATS (id_type,nom,prix) VALUES(2,'Dolma',12.50):
-insert into PLATS (id_type,nom,prix) VALUES(3,'Yaourt grecque',3.50):
+insert into PLATS (id_type,nom,prix,url) VALUES(2,"Burger frites",7.50,"./sources/burgerFrites.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(1,"Avocat Oeufs",2.50,"./sources/duoAvocatOeuf.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(3,"Gateau aux myrtilles",3.50,"./sources/gateauMyrtille.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(3,"Maccarons",3.50,"./sources/maccarons.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(3,"Pancakes",3.50,"./sources/pancakes.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(1,"Salade de riz",3.50,"./sources/saladeRiz.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(1,"Tomates Mozza",3.50,"./sources/tomateMozza.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(2,"Wok épicé",6.50,"./sources/wok.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(2,"Gratin savoyard",8.50,"./sources/gratinSavoyard.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(3,"Mochi",3.50,"./sources/mochi.jpg");
+insert into PLATS (id_type,nom,prix,url) VALUES(1,"Salade composée",3.50,"./sources/salad.jpg");
+
+/* Menus */
+insert into MENUS (nom,description,liste_plats) VALUES("Menu 3000K","Un peu trop maigre? Ce menu vous apportera toutes les calories pour grossir en un éclair!!","{7,1,3}");
+insert into MENUS (nom,description,liste_plats) VALUES("Menu raffiné","Surprenez vos papilles","{2,8,4}");
+insert into MENUS (nom,description,liste_plats) VALUES("Menu asiatique","Voyagez en asie le temps d'un repas","{6,8,10}");
+insert into MENUS (nom,description,liste_plats) VALUES("Menu bien de chez nous","La gastronomie française","{11,9,3}");
+
+/* Cartes */
+insert into CARTES (nom, liste_menus, date_creation) VALUES("Carte 1","{1,2,3}","2018-05-02");
+insert into CARTES (nom, liste_menus, date_creation) VALUES("Carte 2","{1,2,3,4}","2018-02-22");
+insert into CARTES (nom, liste_menus, date_creation) VALUES("Carte 3","{4,2}","2016-04-26");
+
+
--- scripts/create_db.php
+++ scripts/create_db.php
@@ -1,14 +1,9 @@
 <?php
 
-/*
-*Ce fichier va nous permettre d'initialiser nos données en créant la db et les tables qui vont avec
-*Pour cela, les requêtes sont enregistrees dans un fichier nommé database.sql
-*/
-
 //On recupere le contenu du fichier sous forme de chaine de caractères
 $sql = file_get_contents("../db/database.sql");
 
-$dbh = new PDO('mysql:host=localhost;','root', 'password');
+$dbh = new PDO('mysql:host=localhost;','root', 'root');
 
 $dbh->exec($sql);
 echo $sql;
